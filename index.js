ca65916eee34dc9f644f56309c9577329239651a
konateb <111666644+konateb@users.noreply.github.com>
2023-08-13 14:50:14 -0400

    add tp2.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import prompt from "prompt-sync";
 import { add, sub, getRandom1, getRandom2 } from "./tp/tp1.js";
+const obj = {
+  property1: value1, // property name may be an identifier
+  2: value2, // or a number
+  "property n": value3, // or a string
+};
+
+
 //
 //l'appel de fonction getRandom. Math.floor donne les nombres entiers
 let r = Math.floor(getRandom1());
@@ -18,6 +25,9 @@ console.log("la somme : ", somme);
 console.log("la differance: ", difference);
 // ajouter une logique qui demande de tapper le min et le max.
 // ensuite supprimer les // des lignes 23 et 24
-
-// let r2 = Math.floor(getRandom2(min, max));
-// console.log("Aleatoire : ", r2);
+//demande de tapper le premier nombre
+let tempMin = prompt()("taper le premier: ");
+//demande de tapper le second nombre
+let tempMax = prompt()("taper le second: ");
+ let r2 = Math.floor(getRandom2(tempMin, tempMax));
+console.log("Aleatoire : ", r2);
